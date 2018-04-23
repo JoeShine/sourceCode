c40f5cbdf59ce88dc577c697d972779d926dcbcc
JoeShine <2275502840@qq.com>
2018-04-24 01:23:51 +0800

    20180424-0123
    腾讯一站式hello world

--- client/config.js
+++ client/config.js
@@ -12,16 +12,19 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `${host}/weapp/login`,
+        loginUrl: `${host}/login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/weapp/user`,
+        requestUrl: `${host}/user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `${host}/weapp/tunnel`,
-
-        // 上传图片接口
-        uploadUrl: `${host}/weapp/upload`
+        tunnelUrl: `${host}/tunnel`,
+        //获取微信用户的uid
+        uidUrl:`${host}/form/getuid`,
+        //登记URL
+        regUrl:`${host}/form/reg`,
+        //查询
+        getUrl:`${host}/form/get`,
     }
 };
 
---
+++ server/README (2).md
@@ -0,0 +1,4 @@
+# Hello World 服务端
+
+本仓库是 腾讯云小程序服务器解决方案的业务端的一部分 需要配合[php sdk](https://github.com/tencentyun/wafer-php-server-sdk) 一起使用
+这里为了更突出重点，只上传了差异部分，直接对应到PHP SDK对应目录即可。
