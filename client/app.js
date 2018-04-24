a28ab634c2fe62a503042185272c974ccbd105fe
JoeShine <2275502840@qq.com>
2018-04-24 22:16:30 +0800

    20180424-2216
    小程序client端demo覆盖

--- client/app.js
+++ client/app.js
@@ -1,9 +1,38 @@
 //app.js
-var qcloud = require('./vendor/wafer2-client-sdk/index')
-var config = require('./config')
-
 App({
-    onLaunch: function () {
-        qcloud.setLoginUrl(config.service.loginUrl)
+  onLaunch: function() {    
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },  
+  getUserInfo: function(cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+      console.log(this.globalData.userInfo,1)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function(res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+          console.log(that.globalData.userInfo, 1)
+        }
+      })
     }
+  },
+  onLoad: function() {
+    
+  },
+  globalData: {
+    userInfo: null,
+    location: "",
+    city: '',
+    address: '',
+    buycar_num: 0,
+    totalMoney: 0,
+    totalSecond: 899
+  }
 })
