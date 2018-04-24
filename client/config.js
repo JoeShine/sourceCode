13ba0e0be0775bc168125ed36ed4defa30cf70e4
JoeShine <2275502840@qq.com>
2018-04-24 12:57:45 +0800

    Revert "20180424-0123"
    This reverts commit c40f5cbdf59ce88dc577c697d972779d926dcbcc.

--- client/config.js
+++ client/config.js
@@ -12,19 +12,16 @@ var config = {
         host,
 
         // 登录地址，用于建立会话
-        loginUrl: `${host}/login`,
+        loginUrl: `${host}/weapp/login`,
 
         // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/user`,
+        requestUrl: `${host}/weapp/user`,
 
         // 测试的信道服务地址
-        tunnelUrl: `${host}/tunnel`,
-        //获取微信用户的uid
-        uidUrl:`${host}/form/getuid`,
-        //登记URL
-        regUrl:`${host}/form/reg`,
-        //查询
-        getUrl:`${host}/form/get`,
+        tunnelUrl: `${host}/weapp/tunnel`,
+
+        // 上传图片接口
+        uploadUrl: `${host}/weapp/upload`
     }
 };
 
